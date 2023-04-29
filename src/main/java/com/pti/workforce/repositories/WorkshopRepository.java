4981fbfdd979eb3ef8ee2d902b2b434df5d03d1c
Jimmy McCue <xstreamfishn@gmail.com>
2023-04-29 14:13:21 -0400

    added workshop repo

---
+++ src/main/java/com/pti/workforce/repositories/WorkshopRepository.java
@@ -0,0 +1,7 @@
+package com.pti.workforce.repositories;
+
+import com.pti.workforce.entities.Workshop;
+import org.springframework.data.repository.CrudRepository;
+
+public interface WorkshopRepository extends CrudRepository<Workshop, Long> {
+}
